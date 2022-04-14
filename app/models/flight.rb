13b82c96d897a1d31ec53a4e72611a8c8070fd09
Quentin Pongratz <qpongratz@gmail.com>
2022-04-14 18:05:48 -0500

    Create form for looking up flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+  def self.valid_dates
+    select(:date).distinct.map(&:date).sort
+  end
 end
