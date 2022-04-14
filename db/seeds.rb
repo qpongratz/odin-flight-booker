b830439b3837a18cb1773f9646fd718babbe6430
Quentin Pongratz <qpongratz@gmail.com>
2022-04-14 17:06:03 -0500

    Code seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Seeding airports...'
+
+Airport.create(name: 'San Francisco International Airport', code: 'SFO')
+Airport.create(name: 'John F. Kennedy International Airport', code: 'JFK')
+Airport.create(name: 'Will Rogers International Airport', code: 'OKC')
+Airport.create(name: 'Dallas/Fort Worth International Airport', code: 'DFW')
+Airport.create(name: 'Albuquerque "International Sunport', code: 'ABQ')
+
+puts 'Seeding flights...'
+
+200.times do
+  airports = (1..5).to_a.shuffle
+  date = [Date.yesterday, Date.today, Date.tomorrow]
+  Flight.create(arrival_airport_id: airports[0],
+                departure_airport_id: airports[1],
+                date: date.sample,
+                duration: rand(100..500))
+end
+
+puts 'Seeding complete.'
