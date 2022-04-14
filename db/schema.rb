c6e42bb46894a71d44c6f3700e2fc18e154dc4bd
Quentin Pongratz <qpongratz@gmail.com>
2022-04-14 16:59:57 -0500

    Add names to airport

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_214420) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_215923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_214420) do
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "flights", force: :cascade do |t|
