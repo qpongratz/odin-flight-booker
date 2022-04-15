5f236a67171ea7c0b3dc43d13cbd4253d5f02066
Quentin Pongratz <qpongratz@gmail.com>
2022-04-15 18:53:05 -0500

    Finish new and add show on Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,9 +2,29 @@ class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
     @booking = Booking.new
-    @passengers = []
-    params[:passenger_count].to_i.times do
-      @passengers << Passenger.new
+    params[:passenger_count].to_i.times{ @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    
+    if @booking.save
+      flash[:notice] = 'Flight booked'
+      redirect_to @booking
+    else
+      flash[:alert] = 'Oh no!'
+      render new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    @passengers = @booking.passengers
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[name email])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :flights, only: %i[index]
-  resources :bookings, only: %i[new create]
+  resources :bookings, only: %i[new create show]
 
   root 'flights#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
