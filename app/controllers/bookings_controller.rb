90729209251966d93cfffa98eebc0cfc7c889da8
Quentin Pongratz <qpongratz@gmail.com>
2022-04-15 16:54:10 -0500

    Create logic for passenger fields

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,10 @@
 class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
+    @booking = Booking.new
+    @passengers = []
+    params[:passenger_count].to_i.times do
+      @passengers << Passenger.new
+    end
   end
 end
