ac9b7d3cff01068a71bd789cdfb2552234d0dab5
Quentin Pongratz <qpongratz@gmail.com>
2022-04-14 19:24:51 -0500

    Search and display flights correctly

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,10 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @flights = Flight.arrival(params[:arrival])
+                     .departure(params[:departure])
+                     .on_date(params[:date])
+                     .includes(:arrival_airport)
+                     .includes(:departure_airport)
   end
 
   def show
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,10 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
 
+  scope :arrival, ->(id) { where(arrival_airport_id: id) }
+  scope :departure, ->(id) { where(departure_airport_id: id) }
+  scope :on_date, ->(date) { where(date: date) }
+
   def self.valid_dates
     select(:date).distinct.map(&:date).sort
   end
