7c84da13e67630d1fffdbff73250da56359e8d35
Quentin Pongratz <qpongratz@gmail.com>
2022-04-15 16:06:56 -0500

    Add booking model

---
+++ db/migrate/20220415210408_create_bookings.rb
@@ -0,0 +1,10 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.belongs_to :flight, null: false, foreign_key: true
+      t.belongs_to :passenger, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_15_210050) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_15_210408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_210050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "flight_id", null: false
+    t.bigint "passenger_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
+    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -39,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_15_210050) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookings", "flights"
+  add_foreign_key "bookings", "passengers"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
