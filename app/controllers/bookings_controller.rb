5c804eafa227c6bc46bc23edfca924f2ad36ca53
Quentin Pongratz <qpongratz@gmail.com>
2022-04-15 21:47:27 -0500

    Fix minor formatting

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,7 +7,7 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    
+
     if @booking.save
       flash[:notice] = 'Flight booked'
       redirect_to @booking
