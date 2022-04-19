3b9ced65d513abe14c7dc9430fb425923bd8dbd4
Quentin Pongratz <qpongratz@gmail.com>
2022-04-19 15:50:05 -0500

    Add email testing

--- test/mailers/previews/passenger_mailer_preview.rb
+++ test/mailers/previews/passenger_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/passenger_mailer
 class PassengerMailerPreview < ActionMailer::Preview
 
+  def booking_confirmation
+    passenger = Passenger.last
+    PassengerMailer.with(passenger: passenger).booking_confirmation
+  end
 end
