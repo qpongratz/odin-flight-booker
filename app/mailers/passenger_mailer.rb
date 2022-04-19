b3b61c58062341e2edcdc96674063931d977723c
Quentin Pongratz <qpongratz@gmail.com>
2022-04-19 14:54:11 -0500

    Add logic to passenger mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def booking_confirmation
+    @passenger = params[:passenger]
+    @flight = @passenger.flight
+    mail(to: @passenger.email, subject: 'Booking Confirmation')
+  end
 end
